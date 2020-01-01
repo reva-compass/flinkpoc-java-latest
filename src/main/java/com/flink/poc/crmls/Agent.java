ff3e56614279c5d743ddda07f70695909681509e
reva-compass <rkandoji@compass.com>
2020-01-01 18:52:18 -0500

    CRMLS integration for lisings and agents, and respective joins

---
+++ src/main/java/com/flink/poc/crmls/Agent.java
@@ -0,0 +1,71 @@
+package com.flink.poc.crmls;
+
+public class Agent {
+
+    private String ucPK;
+    private String ucUpdateTS;
+    private String ucVersion;
+    private String ucRowType;
+    private String ucType;
+    private String data;
+
+    public String getUcPK() {
+        return ucPK;
+    }
+
+    public void setUcPK(String ucPK) {
+        this.ucPK = ucPK;
+    }
+
+    public String getUcUpdateTS() {
+        return ucUpdateTS;
+    }
+
+    public void setUcUpdateTS(String ucUpdateTS) {
+        this.ucUpdateTS = ucUpdateTS;
+    }
+
+    public String getUcVersion() {
+        return ucVersion;
+    }
+
+    public void setUcVersion(String ucVersion) {
+        this.ucVersion = ucVersion;
+    }
+
+    public String getUcRowType() {
+        return ucRowType;
+    }
+
+    public void setUcRowType(String ucRowType) {
+        this.ucRowType = ucRowType;
+    }
+
+    public String getUcType() {
+        return ucType;
+    }
+
+    public void setUcType(String ucType) {
+        this.ucType = ucType;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Agent{" +
+                "ucPK='" + ucPK + '\'' +
+                ", ucUpdateTS='" + ucUpdateTS + '\'' +
+                ", ucVersion='" + ucVersion + '\'' +
+                ", ucRowType='" + ucRowType + '\'' +
+                ", ucType='" + ucType + '\'' +
+                ", data='" + data + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/flink/poc/crmls/CrmlsJoiner.java
@@ -0,0 +1,179 @@
+package com.flink.poc.crmls;
+
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.util.serialization.JSONKeyValueDeserializationSchema;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.java.StreamTableEnvironment;
+import org.apache.flink.types.Row;
+
+import java.util.Properties;
+
+
+public class CrmlsJoiner {
+
+    private static StreamExecutionEnvironment bsEnv = StreamExecutionEnvironment.getExecutionEnvironment();
+    private static EnvironmentSettings bsSettings = EnvironmentSettings.newInstance().useBlinkPlanner().inStreamingMode().build();
+    private static StreamTableEnvironment bsTableEnv = StreamTableEnvironment.create(bsEnv, bsSettings);
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    private static Properties setProps() {
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "b-2.listings-infra-dev-191.lguuvv.c6.kafka.us-east-1.amazonaws.com:9092," +
+                "b-1.listings-infra-dev-191.lguuvv.c6.kafka.us-east-1.amazonaws.com:9092," +
+                "b-3.listings-infra-dev-191.lguuvv.c6.kafka.us-east-1.amazonaws.com:9092");
+        return properties;
+    }
+
+    private static Table processListings() {
+        Properties properties = setProps();
+        FlinkKafkaConsumer<ObjectNode> kafkaConsumer =
+                new FlinkKafkaConsumer<>("la_crmls_rets-listings-neo", new JSONKeyValueDeserializationSchema(true), properties);
+        kafkaConsumer.setStartFromEarliest();
+        DataStream<Listing> listingStream = bsEnv.addSource(kafkaConsumer).map((MapFunction<ObjectNode, Listing>) jsonNodes -> {
+            JsonNode jsonNode = jsonNodes.get("value");
+            Listing listing = new Listing();
+            if (jsonNode.has("uc_pk"))
+                listing.setUcPK(jsonNode.get("uc_pk").textValue());
+            if (jsonNode.has("uc_update_ts"))
+                listing.setUcUpdateTS(jsonNode.get("uc_update_ts").textValue());
+            if (jsonNode.has("uc_version"))
+                listing.setUcVersion(jsonNode.get("uc_version").textValue());
+            if (jsonNode.has("uc_row_type"))
+                listing.setUcRowType(jsonNode.get("uc_row_type").textValue());
+            if (jsonNode.has("uc_type"))
+                listing.setUcType(jsonNode.get("uc_type").textValue());
+            String dataStr = jsonNode.get("data").textValue();
+            JsonNode dataNode = mapper.readTree(dataStr);
+            listing.setData(dataStr);
+            if (dataNode.has("ListingKeyNumeric"))
+                listing.setListingKey(dataNode.get("ListingKeyNumeric").textValue());
+            if (dataNode.has("ListAgentKeyNumeric"))
+                listing.setListAgentKey(dataNode.get("ListAgentKeyNumeric").textValue());
+            if (dataNode.has("BuyerAgentKeyNumeric"))
+                listing.setBuyerAgentKey(dataNode.get("BuyerAgentKeyNumeric").textValue());
+            if (dataNode.has("CoListAgentKeyNumeric"))
+                listing.setCoListAgentKey(dataNode.get("CoListAgentKeyNumeric").textValue());
+            if (dataNode.has("CoBuyerAgentKeyNumeric"))
+                listing.setCoBuyerAgentKey(dataNode.get("CoBuyerAgentKeyNumeric").textValue());
+            if (dataNode.has("ListOfficeKeyNumeric"))
+                listing.setListOfficeKey(dataNode.get("ListOfficeKeyNumeric").textValue());
+            if (dataNode.has("BuyerOfficeKeyNumeric"))
+                listing.setBuyerOfficeKey(dataNode.get("BuyerOfficeKeyNumeric").textValue());
+            if (dataNode.has("CoListOfficeKeyNumeric"))
+                listing.setCoListOfficeKey(dataNode.get("CoListOfficeKeyNumeric").textValue());
+            if (dataNode.has("CoBuyerOfficeKeyNumeric"))
+                listing.setCoBuyerOfficeKey(dataNode.get("CoBuyerOfficeKeyNumeric").textValue());
+            //  System.out.println("### list obj " + listing);
+            return listing;
+        });
+
+        bsTableEnv.registerDataStream("Listings", listingStream, "ucPK, " +
+                "ucUpdateTS, " +
+                "ucVersion, " +
+                "ucRowType, " +
+                "ucType, " +
+                "listingKey, " +
+                "listAgentKey, " +
+                "buyerAgentKey, " +
+                "coListAgentKey, " +
+                "coBuyerAgentKey, " +
+                "listOfficeKey, " +
+                "buyerOfficeKey, " +
+                "coListOfficeKey, " +
+                "coBuyerOfficeKey, " +
+                "data," +
+                "proctime.proctime");
+
+        Table latestListingsTbl = bsTableEnv.sqlQuery("SELECT * FROM (" +
+                "SELECT *, ROW_NUMBER() " +
+                "OVER (PARTITION BY listingKey ORDER BY proctime DESC) " +
+                "AS row_num FROM Listings)" +
+                "WHERE row_num = 1");
+        return latestListingsTbl;
+    }
+
+    private static Table processAgents() {
+        Properties properties = setProps();
+        FlinkKafkaConsumer<ObjectNode> agentKafkaConsumer = new FlinkKafkaConsumer<>("la_crmls_rets-agents-neo", new JSONKeyValueDeserializationSchema(true), properties);
+        agentKafkaConsumer.setStartFromEarliest();
+        DataStream<Agent> agentStream = bsEnv.addSource(agentKafkaConsumer).map((MapFunction<ObjectNode, Agent>) jsonNodes -> {
+            JsonNode jsonNode = jsonNodes.get("value");
+            Agent agent = new Agent();
+            if (jsonNode.has("uc_pk"))
+                agent.setUcPK(jsonNode.get("uc_pk").textValue());
+            if (jsonNode.has("uc_update_ts"))
+                agent.setUcUpdateTS(jsonNode.get("uc_update_ts").textValue());
+            if (jsonNode.has("uc_version"))
+                agent.setUcVersion(jsonNode.get("uc_version").textValue());
+            if (jsonNode.has("uc_row_type"))
+                agent.setUcRowType(jsonNode.get("uc_row_type").textValue());
+            if (jsonNode.has("uc_type"))
+                agent.setUcType(jsonNode.get("uc_type").textValue());
+            agent.setData(jsonNode.get("data").textValue());
+            return agent;
+        });
+
+        bsTableEnv.registerDataStream("Agents", agentStream, "ucPK, " +
+                "ucUpdateTS, " +
+                "ucVersion, " +
+                "ucRowType, " +
+                "ucType, " +
+                "data," +
+                "proctime.proctime");
+
+        Table latestAgentsTbl = bsTableEnv.sqlQuery("SELECT * FROM (" +
+                "SELECT *, ROW_NUMBER() " +
+                "OVER (PARTITION BY ucPK ORDER BY proctime DESC) " +
+                "AS row_num FROM Agents)" +
+                "WHERE row_num = 1");
+
+        return latestAgentsTbl;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("### inside main");
+
+        /**
+         * LISTINGS
+         */
+        Table latestListingsTbl = processListings();
+        bsTableEnv.registerTable("latestListings", latestListingsTbl);
+//        Table result3 = bsTableEnv.sqlQuery(
+//                "SELECT * FROM latestListings");
+//        bsTableEnv.toRetractStream(result3, Row.class).print();
+
+        /**
+         * AGENTS
+         */
+        Table latestAgentsTbl = processAgents();
+        bsTableEnv.registerTable("latestAgents", latestAgentsTbl);
+//        Table result4 = bsTableEnv.sqlQuery(
+//                "SELECT * FROM latestAgents");
+//        bsTableEnv.toRetractStream(result4, Row.class).print();
+
+        /**
+         * JOIN
+         */
+        Table joinedTbl = bsTableEnv.sqlQuery(
+                "SELECT * FROM latestListings l " +
+                        "LEFT JOIN latestAgents aa ON l.listAgentKey = aa.ucPK " +
+                        "LEFT JOIN latestAgents ab ON l.buyerAgentKey = ab.ucPK " +
+                        "LEFT JOIN latestAgents ac ON l.coListAgentKey = ac.ucPK " +
+                        "LEFT JOIN latestAgents ad ON l.coBuyerAgentKey = ad.ucPK"
+        );
+        bsTableEnv.toRetractStream(joinedTbl, Row.class).print();
+
+        bsEnv.execute("test-job");
+
+    }
+
+
+}
