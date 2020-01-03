df1650bff9874ed8ac509bde08efe1f7d96174aa
reva-compass <rkandoji@compass.com>
2020-01-03 17:11:32 -0500

    adding key to kafka messages

--- src/main/java/com/flink/poc/crmls/JoinedSerializer.java
+++ src/main/java/com/flink/poc/crmls/JoinedSerializer.java
@@ -1,6 +1,5 @@
 package com.flink.poc.crmls;
 
-import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.connectors.kafka.KafkaSerializationSchema;
 import org.apache.flink.types.Row;
@@ -18,6 +17,7 @@ public class JoinedSerializer implements KafkaSerializationSchema<Tuple2<Boolean
 
     @Override
     public org.apache.kafka.clients.producer.ProducerRecord<byte[], byte[]> serialize(Tuple2<Boolean, Row> booleanRowTuple2, Long aLong) {
-        return new org.apache.kafka.clients.producer.ProducerRecord<byte[], byte[]>(topic, booleanRowTuple2.toString().getBytes(StandardCharsets.UTF_8));
+        String[] obj = booleanRowTuple2.getField(1).toString().split(",");
+        return new org.apache.kafka.clients.producer.ProducerRecord<byte[], byte[]>(topic, obj[0].getBytes(), booleanRowTuple2.toString().getBytes(StandardCharsets.UTF_8));
     }
 }
