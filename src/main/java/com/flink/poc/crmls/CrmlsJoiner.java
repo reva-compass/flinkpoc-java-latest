92336a157d30682c97f016bf482a3e007dd43527
reva-compass <rkandoji@compass.com>
2020-01-13 15:17:50 -0500

    adding unique column names

--- src/main/java/com/flink/poc/crmls/CrmlsJoiner.java
+++ src/main/java/com/flink/poc/crmls/CrmlsJoiner.java
@@ -79,98 +79,95 @@ public class CrmlsJoiner {
             JsonNode jsonNode = jsonNodes.get("value");
             Listing listing = new Listing();
             if (jsonNode.has("uc_pk"))
-                listing.setUcPK(jsonNode.get("uc_pk").textValue());
+                listing.setUcPKL(jsonNode.get("uc_pk").textValue());
             if (jsonNode.has("uc_update_ts"))
-                listing.setUcUpdateTS(jsonNode.get("uc_update_ts").textValue());
+                listing.setUcUpdateTSL(jsonNode.get("uc_update_ts").textValue());
             if (jsonNode.has("uc_version"))
-                listing.setUcVersion(jsonNode.get("uc_version").textValue());
+                listing.setUcVersionL(jsonNode.get("uc_version").textValue());
             if (jsonNode.has("uc_row_type"))
-                listing.setUcRowType(jsonNode.get("uc_row_type").textValue());
+                listing.setUcRowTypeL(jsonNode.get("uc_row_type").textValue());
             if (jsonNode.has("uc_type"))
-                listing.setUcType(jsonNode.get("uc_type").textValue());
+                listing.setUcTypeL(jsonNode.get("uc_type").textValue());
             String dataStr = jsonNode.get("data").textValue();
             JsonNode dataNode = mapper.readTree(dataStr);
-            listing.setData(dataStr);
-            if (dataNode.has("ListingKeyNumeric"))
-                listing.setListingKey(dataNode.get("ListingKeyNumeric").textValue());
+            listing.setDataL(dataStr);
 
             if (dataNode.has("ListAgentKeyNumeric")) {
-                listing.setListAgentKey(dataNode.get("ListAgentKeyNumeric").textValue());
+                listing.setListAgentKeyL(dataNode.get("ListAgentKeyNumeric").textValue());
             } else {
                 Integer x = 100000 + rnd.nextInt(900000);
-                listing.setListAgentKey(x + COMPASS_NULL);
+                listing.setListAgentKeyL(x + COMPASS_NULL);
             }
             if (dataNode.has("BuyerAgentKeyNumeric")) {
-                listing.setBuyerAgentKey(dataNode.get("BuyerAgentKeyNumeric").textValue());
+                listing.setBuyerAgentKeyL(dataNode.get("BuyerAgentKeyNumeric").textValue());
             } else {
                 Integer x = 100000 + rnd.nextInt(900000);
-                listing.setBuyerAgentKey(x + COMPASS_NULL);
+                listing.setBuyerAgentKeyL(x + COMPASS_NULL);
             }
 
             if (dataNode.has("CoListAgentKeyNumeric")) {
-                listing.setCoListAgentKey(dataNode.get("CoListAgentKeyNumeric").textValue());
+                listing.setCoListAgentKeyL(dataNode.get("CoListAgentKeyNumeric").textValue());
             } else {
                 Integer x = 100000 + rnd.nextInt(900000);
-                listing.setCoListAgentKey(x + COMPASS_NULL);
+                listing.setCoListAgentKeyL(x + COMPASS_NULL);
             }
 
             if (dataNode.has("CoBuyerAgentKeyNumeric")) {
-                listing.setCoBuyerAgentKey(dataNode.get("CoBuyerAgentKeyNumeric").textValue());
+                listing.setCoBuyerAgentKeyL(dataNode.get("CoBuyerAgentKeyNumeric").textValue());
             } else {
                 Integer x = 100000 + rnd.nextInt(900000);
-                listing.setCoBuyerAgentKey(x + COMPASS_NULL);
+                listing.setCoBuyerAgentKeyL(x + COMPASS_NULL);
             }
 
             if (dataNode.has("ListOfficeKeyNumeric")) {
-                listing.setListOfficeKey(dataNode.get("ListOfficeKeyNumeric").textValue());
+                listing.setListOfficeKeyL(dataNode.get("ListOfficeKeyNumeric").textValue());
             } else {
                 Integer x = 100000 + rnd.nextInt(900000);
-                listing.setListOfficeKey(x + COMPASS_NULL);
+                listing.setListOfficeKeyL(x + COMPASS_NULL);
             }
             if (dataNode.has("BuyerOfficeKeyNumeric")) {
-                listing.setBuyerOfficeKey(dataNode.get("BuyerOfficeKeyNumeric").textValue());
+                listing.setBuyerOfficeKeyL(dataNode.get("BuyerOfficeKeyNumeric").textValue());
             } else {
                 Integer x = 100000 + rnd.nextInt(900000);
-                listing.setBuyerOfficeKey(x + COMPASS_NULL);
+                listing.setBuyerOfficeKeyL(x + COMPASS_NULL);
             }
 
             if (dataNode.has("CoListOfficeKeyNumeric")) {
-                listing.setCoListOfficeKey(dataNode.get("CoListOfficeKeyNumeric").textValue());
+                listing.setCoListOfficeKeyL(dataNode.get("CoListOfficeKeyNumeric").textValue());
             } else {
                 Integer x = 100000 + rnd.nextInt(900000);
-                listing.setCoListOfficeKey(x + COMPASS_NULL);
+                listing.setCoListOfficeKeyL(x + COMPASS_NULL);
             }
 
             if (dataNode.has("CoBuyerOfficeKeyNumeric")) {
-                listing.setCoBuyerOfficeKey(dataNode.get("CoBuyerOfficeKeyNumeric").textValue());
+                listing.setCoBuyerOfficeKeyL(dataNode.get("CoBuyerOfficeKeyNumeric").textValue());
             } else {
                 Integer x = 100000 + rnd.nextInt(900000);
-                listing.setCoBuyerOfficeKey(x + COMPASS_NULL);
+                listing.setCoBuyerOfficeKeyL(x + COMPASS_NULL);
             }
             //  System.out.println("### list obj " + listing);
             return listing;
         }).name("Source: Listings");
 
-        bsTableEnv.registerDataStream("Listings", listingStream, "ucPK, " +
-                "ucUpdateTS, " +
-                "ucVersion, " +
-                "ucRowType, " +
-                "ucType, " +
-                "listingKey, " +
-                "listAgentKey, " +
-                "buyerAgentKey, " +
-                "coListAgentKey, " +
-                "coBuyerAgentKey, " +
-                "listOfficeKey, " +
-                "buyerOfficeKey, " +
-                "coListOfficeKey, " +
-                "coBuyerOfficeKey, " +
-                "data," +
+        bsTableEnv.registerDataStream("Listings", listingStream, "ucPKL, " +
+                "ucUpdateTSL, " +
+                "ucVersionL, " +
+                "ucRowTypeL, " +
+                "ucTypeL, " +
+                "listAgentKeyL, " +
+                "buyerAgentKeyL, " +
+                "coListAgentKeyL, " +
+                "coBuyerAgentKeyL, " +
+                "listOfficeKeyL, " +
+                "buyerOfficeKeyL, " +
+                "coListOfficeKeyL, " +
+                "coBuyerOfficeKeyL, " +
+                "dataL," +
                 "proctime.proctime");
 
         Table latestListingsTbl = bsTableEnv.sqlQuery("SELECT * FROM (" +
                 "SELECT *, ROW_NUMBER() " +
-                "OVER (PARTITION BY listingKey ORDER BY proctime DESC) " +
+                "OVER (PARTITION BY ucPKL ORDER BY proctime DESC) " +
                 "AS row_num FROM Listings)" +
                 "WHERE row_num = 1");
         return latestListingsTbl;
@@ -185,30 +182,30 @@ public class CrmlsJoiner {
             JsonNode jsonNode = jsonNodes.get("value");
             Agent agent = new Agent();
             if (jsonNode.has("uc_pk"))
-                agent.setUcPK(jsonNode.get("uc_pk").textValue());
+                agent.setUcPKA(jsonNode.get("uc_pk").textValue());
             if (jsonNode.has("uc_update_ts"))
-                agent.setUcUpdateTS(jsonNode.get("uc_update_ts").textValue());
+                agent.setUcUpdateTSA(jsonNode.get("uc_update_ts").textValue());
             if (jsonNode.has("uc_version"))
-                agent.setUcVersion(jsonNode.get("uc_version").textValue());
+                agent.setUcVersionA(jsonNode.get("uc_version").textValue());
             if (jsonNode.has("uc_row_type"))
-                agent.setUcRowType(jsonNode.get("uc_row_type").textValue());
+                agent.setUcRowTypeA(jsonNode.get("uc_row_type").textValue());
             if (jsonNode.has("uc_type"))
-                agent.setUcType(jsonNode.get("uc_type").textValue());
-            agent.setData(jsonNode.get("data").textValue());
+                agent.setUcTypeA(jsonNode.get("uc_type").textValue());
+            agent.setDataA(jsonNode.get("data").textValue());
             return agent;
         }).name("Source: Agents");
 
-        bsTableEnv.registerDataStream("Agents", agentStream, "ucPK, " +
-                "ucUpdateTS, " +
-                "ucVersion, " +
-                "ucRowType, " +
-                "ucType, " +
-                "data," +
+        bsTableEnv.registerDataStream("Agents", agentStream, "ucPKA, " +
+                "ucUpdateTSA, " +
+                "ucVersionA, " +
+                "ucRowTypeA, " +
+                "ucTypeA, " +
+                "dataA," +
                 "proctime.proctime");
 
         Table latestAgentsTbl = bsTableEnv.sqlQuery("SELECT * FROM (" +
                 "SELECT *, ROW_NUMBER() " +
-                "OVER (PARTITION BY ucPK ORDER BY proctime DESC) " +
+                "OVER (PARTITION BY ucPKA ORDER BY proctime DESC) " +
                 "AS row_num FROM Agents)" +
                 "WHERE row_num = 1");
 
@@ -224,30 +221,30 @@ public class CrmlsJoiner {
             JsonNode jsonNode = jsonNodes.get("value");
             Office office = new Office();
             if (jsonNode.has("uc_pk"))
-                office.setUcPK(jsonNode.get("uc_pk").textValue());
+                office.setUcPKO(jsonNode.get("uc_pk").textValue());
             if (jsonNode.has("uc_update_ts"))
-                office.setUcUpdateTS(jsonNode.get("uc_update_ts").textValue());
+                office.setUcUpdateTSO(jsonNode.get("uc_update_ts").textValue());
             if (jsonNode.has("uc_version"))
-                office.setUcVersion(jsonNode.get("uc_version").textValue());
+                office.setUcVersionO(jsonNode.get("uc_version").textValue());
             if (jsonNode.has("uc_row_type"))
-                office.setUcRowType(jsonNode.get("uc_row_type").textValue());
+                office.setUcRowTypeO(jsonNode.get("uc_row_type").textValue());
             if (jsonNode.has("uc_type"))
-                office.setUcType(jsonNode.get("uc_type").textValue());
-            office.setData(jsonNode.get("data").textValue());
+                office.setUcTypeO(jsonNode.get("uc_type").textValue());
+            office.setDataO(jsonNode.get("data").textValue());
             return office;
         }).name("Source: Offices");
 
-        bsTableEnv.registerDataStream("Offices", officeStream, "ucPK, " +
-                "ucUpdateTS, " +
-                "ucVersion, " +
-                "ucRowType, " +
-                "ucType, " +
-                "data," +
+        bsTableEnv.registerDataStream("Offices", officeStream, "ucPKO, " +
+                "ucUpdateTSO, " +
+                "ucVersionO, " +
+                "ucRowTypeO, " +
+                "ucTypeO, " +
+                "dataO," +
                 "proctime.proctime");
 
         Table latestOfficesTbl = bsTableEnv.sqlQuery("SELECT * FROM (" +
                 "SELECT *, ROW_NUMBER() " +
-                "OVER (PARTITION BY ucPK ORDER BY proctime DESC) " +
+                "OVER (PARTITION BY ucPKO ORDER BY proctime DESC) " +
                 "AS row_num FROM Offices)" +
                 "WHERE row_num = 1");
 
@@ -263,35 +260,35 @@ public class CrmlsJoiner {
             JsonNode jsonNode = jsonNodes.get("value");
             OpenHouse openHouse = new OpenHouse();
             if (jsonNode.has("uc_pk"))
-                openHouse.setUcPK(jsonNode.get("uc_pk").textValue());
+                openHouse.setUcPKOH(jsonNode.get("uc_pk").textValue());
             if (jsonNode.has("uc_update_ts"))
-                openHouse.setUcUpdateTS(jsonNode.get("uc_update_ts").textValue());
+                openHouse.setUcUpdateTSOH(jsonNode.get("uc_update_ts").textValue());
             if (jsonNode.has("uc_version"))
-                openHouse.setUcVersion(jsonNode.get("uc_version").textValue());
+                openHouse.setUcVersionOH(jsonNode.get("uc_version").textValue());
             if (jsonNode.has("uc_row_type"))
-                openHouse.setUcRowType(jsonNode.get("uc_row_type").textValue());
+                openHouse.setUcRowTypeOH(jsonNode.get("uc_row_type").textValue());
             if (jsonNode.has("uc_type"))
-                openHouse.setUcType(jsonNode.get("uc_type").textValue());
+                openHouse.setUcTypeOH(jsonNode.get("uc_type").textValue());
             String dataStr = jsonNode.get("data").textValue();
             JsonNode dataNode = mapper.readTree(dataStr);
-            openHouse.setData(dataStr);
+            openHouse.setDataOH(dataStr);
             if (dataNode.has("ListingKeyNumeric"))
-                openHouse.setListingKey(dataNode.get("ListingKeyNumeric").textValue());
+                openHouse.setListingKeyOH(dataNode.get("ListingKeyNumeric").textValue());
             return openHouse;
         }).name("Source: OpenHouses");
 
-        bsTableEnv.registerDataStream("OpenHouses", openHouseStream, "ucPK, " +
-                "ucUpdateTS, " +
-                "ucVersion, " +
-                "ucRowType, " +
-                "ucType, " +
-                "listingKey, " +
-                "data," +
+        bsTableEnv.registerDataStream("OpenHouses", openHouseStream, "ucPKOH, " +
+                "ucUpdateTSOH, " +
+                "ucVersionOH, " +
+                "ucRowTypeOH, " +
+                "ucTypeOH, " +
+                "listingKeyOH, " +
+                "dataOH," +
                 "proctime.proctime");
 
         Table latestOpenHousesTbl = bsTableEnv.sqlQuery("SELECT * FROM (" +
                 "SELECT *, ROW_NUMBER() " +
-                "OVER (PARTITION BY ucPK ORDER BY proctime DESC) " +
+                "OVER (PARTITION BY ucPKOH ORDER BY proctime DESC) " +
                 "AS row_num FROM OpenHouses)" +
                 "WHERE row_num = 1");
 
@@ -343,15 +340,15 @@ public class CrmlsJoiner {
          */
         Table joinedTbl = bsTableEnv.sqlQuery(
                 "SELECT * FROM latestListings l " +
-                        "LEFT JOIN latestAgents aa ON l.listAgentKey = aa.ucPK " +
-                        "LEFT JOIN latestAgents ab ON l.buyerAgentKey = ab.ucPK " +
-                        "LEFT JOIN latestAgents ac ON l.coListAgentKey = ac.ucPK " +
-                        "LEFT JOIN latestAgents ad ON l.coBuyerAgentKey = ad.ucPK " +
-                        "LEFT JOIN latestOffices oa ON l.listOfficeKey = oa.ucPK " +
-                        "LEFT JOIN latestOffices ob ON l.buyerOfficeKey = ob.ucPK " +
-                        "LEFT JOIN latestOffices oc ON l.coListOfficeKey = oc.ucPK " +
-                        "LEFT JOIN latestOffices od ON l.coBuyerOfficeKey = od.ucPK " +
-                        "LEFT JOIN latestOpenHouses oh ON l.listingKey = oh.listingKey"
+                        "LEFT JOIN latestAgents aa ON l.listAgentKeyL = aa.ucPKA " +
+                        "LEFT JOIN latestAgents ab ON l.buyerAgentKeyL = ab.ucPKA " +
+                        "LEFT JOIN latestAgents ac ON l.coListAgentKeyL = ac.ucPKA " +
+                        "LEFT JOIN latestAgents ad ON l.coBuyerAgentKeyL = ad.ucPKA " +
+                        "LEFT JOIN latestOffices oa ON l.listOfficeKeyL = oa.ucPKO " +
+                        "LEFT JOIN latestOffices ob ON l.buyerOfficeKeyL = ob.ucPKO " +
+                        "LEFT JOIN latestOffices oc ON l.coListOfficeKeyL = oc.ucPKO " +
+                        "LEFT JOIN latestOffices od ON l.coBuyerOfficeKeyL = od.ucPKO " +
+                        "LEFT JOIN latestOpenHouses oh ON l.ucPKL = oh.listingKeyOH"
         );
         //       bsTableEnv.toRetractStream(joinedTbl, Row.class).print();
         DataStream<Tuple2<Boolean, Row>> joinedStream = bsTableEnv.toRetractStream(joinedTbl, Row.class);
